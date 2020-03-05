bf3b1f6d9799ea09a7f559970b4041efbb370407
Terrance Brown <terrancebrown@Terrances-MacBook-Pro.local>
2020-03-05 13:44:33 -0500

    yep

---
+++ FunkyBeaver/src/main/java/com/revature/FunkyBeaver/FunkyBeaverServlet.java
@@ -0,0 +1,27 @@
+package com.revature.FunkyBeaver;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FunkyBeaverServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2320938814852603607L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		resp.getWriter().write("FUNKY BEAVER");
+		super.doGet(req, resp);
+	}
+ 
+	
+
+
+}
