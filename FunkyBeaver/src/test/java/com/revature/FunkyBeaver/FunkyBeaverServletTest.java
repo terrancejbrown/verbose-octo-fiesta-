989c205299ea055118ffae102d1e3a367d1c361f
Terrance Brown <terrancebrown@Terrances-MacBook-Pro.local>
2020-03-05 13:57:28 -0500

    yep

---
+++ FunkyBeaver/src/test/java/com/revature/FunkyBeaver/FunkyBeaverServletTest.java
@@ -0,0 +1,14 @@
+package com.revature.FunkyBeaver;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FunkyBeaverServletTest {
+
+	@Test
+	public void test() {
+		assertEquals("True is true", true, true);
+	}
+
+}
